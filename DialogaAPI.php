85c94725a9f6167cd5bb1cdb561be66d7cac0a53
Maurilio Atila <cabelotaina@gmail.com>
2016-02-05 12:40:53 -0200

    fix get online proposition

--- DialogaAPI.php
+++ DialogaAPI.php
@@ -14,14 +14,14 @@ class DialogaAPI
         // TODO: insert total of elements - 88 for pagination - 9 pages. See information on header Total.
 
         // main dev - get by proposals TODO: test with apiUrl
-        //$request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=' . $page  . '&per_page=10');
-        //  $request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=1&per_page=10');
-        //  $response = wp_remote_retrieve_body( $request );
-        //  $proposals = json_decode($response)->{'proposals'};
+        //  $request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=' . $page  . '&per_page=10');
+        $request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=1&per_page=10');
+        $response = wp_remote_retrieve_body( $request );
+        $proposals = json_decode($response)->{'proposals'};
      
         // only for development without connection 
-        $response = file_get_contents('/home/cabelo/apps/dialoga_proposals.json');
-        $proposals = json_decode($response)->{'proposals'};
+        // $response = file_get_contents('/home/cabelo/apps/dialoga_proposals.json');
+        // $proposals = json_decode($response)->{'proposals'};
          
         return $proposals;
     }
