31b017a29089c64ae106efc148bc39fd39714aa7
Jacson Passold <jacson@ethymos.com.br>
2016-02-05 13:04:10 -0200

    error report and check

--- DialogaAPI.php
+++ DialogaAPI.php
@@ -17,12 +17,29 @@ class DialogaAPI
         //  $request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=' . $page  . '&per_page=10');
         $request = wp_remote_get($this->apiUrl . 'proposals_discussion_plugin/121501/ranking?page=1&per_page=10');
         $response = wp_remote_retrieve_body( $request );
-        $proposals = json_decode($response)->{'proposals'};
+        
+        $proposals = array();
+        if(is_object($response))
+        {	
+        	$proposals = json_decode($response)->{'proposals'};
+        }
+        else 
+        {
+        	var_dump($response); //TODO error report
+        }
+        
      
         // only for development without connection 
-        // $response = file_get_contents('/home/cabelo/apps/dialoga_proposals.json');
-        // $proposals = json_decode($response)->{'proposals'};
+        //$response = file_get_contents(dirname(__FILE__).'/dialoga_proposals.json');
+        //$proposals = json_decode($response)->{'proposals'};
          
+        if(!is_array($proposals))
+        {
+        	var_dump($proposals); //TODO error report
+        	
+        	$proposals = array();
+        }
+        
         return $proposals;
     }
 }
